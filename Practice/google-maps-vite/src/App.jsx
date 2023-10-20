bdc180dee8354d764709f50448f02da9d91ba593
Soumya Vaidya <soumyavaidya2016@gmail.com>
2023-10-21 03:00:36 +0530

    User Location & Marker

--- Practice/google-maps-vite/src/App.jsx
+++ Practice/google-maps-vite/src/App.jsx
@@ -1,29 +1,90 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
 
 const containerStyle = {
   width: "400px",
   height: "400px",
 };
 
-const center = {
-  lat: 37.7749, // San Francisco, CA
-  lng: -122.4194,
-};
+
 
 
 function App() {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
+
+
+  const [userLocation, setUserLocation] = useState(null);
+
+  const mapOptions = {
+    zoom: 15,
+  };
+
+  const [locationPermission, setLocationPermission] = useState(null);
+
+  useEffect(() => {
+    // Check for geolocation permission when the component mounts
+    checkLocationPermission();
+
+    // Add an event listener to update the permission status when it changes
+    const permissionChangeListener = () => {
+      checkLocationPermission();
+    };
+
+    navigator.permissions.query({ name: 'geolocation' }).then((result) => {
+      result.onchange = permissionChangeListener;
+    });
+
+    return () => {
+      // Cleanup by removing the event listener
+      navigator.permissions.query({ name: 'geolocation' }).then((result) => {
+        result.onchange = null;
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      if (locationPermission == 'denied') {
+        alert('why u deny');
+      }
+      console.log('prompt shown');
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log('prompt accepted');
+        const userLat = position.coords.latitude;
+        const userLng = position.coords.longitude;
+        setUserLocation({ lat: userLat, lng: userLng });
+
+      });
+
+    }
+  }, [locationPermission]);
+
+  const checkLocationPermission = async () => {
+    try {
+      const { state } = await navigator.permissions.query({ name: 'geolocation' });
+
+      if (state === 'granted') {
+        setLocationPermission('granted');
+      } else if (state === 'prompt') {
+        setLocationPermission('prompt');
+      } else {
+        setLocationPermission('denied');
+      }
+    } catch (error) {
+      console.error('Error checking location permission:', error);
+    }
+  };
 
   return (
-    <>
-      <LoadScript googleMapsApiKey="AIzaSyBM5t4s4-K5hTOhW60ql7p3tUSyCurudWU">
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-          <Marker position={center} />
-        </GoogleMap>
-      </LoadScript>
-    </>
-  )
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap mapContainerStyle={containerStyle} center={userLocation} zoom={mapOptions.zoom} >
+        {userLocation && <Marker position={userLocation} />}
+      </GoogleMap>
+    </LoadScript>
+  );
 }
 
 export default App
--- Practice/google-maps-vite/src/main.jsx
+++ Practice/google-maps-vite/src/main.jsx
@@ -1,10 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <App />
+
 )
